dc58d51693ce5ee51c66d462be68a8408e6808df
Santi0699 <santisabsol06@gmail.com>
2025-04-08 11:32:39 -0300

    TDA listas

---
+++ Listas.h
@@ -0,0 +1,23 @@
+#ifndef LISTAS_H_INCLUDED
+#define MI_HEADER_INCLUDED
+
+#include <stdlib.h>
+#include <stdio.h>
+#define t_elem_node int
+
+typedef struct _node
+{
+    t_elem_node data;
+    struct _node* next;    
+    
+}node;
+
+
+
+
+
+
+
+
+
+#endif
